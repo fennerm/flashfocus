5875ca375dc71d24717fd0dc222f7aa814283662
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-03-06 09:12:24 -0800

    Separated into modules

---
+++ i3flash/i3flash.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+'''Flash i3 windows on focus'''
+import logging
+from logging import info as log
+import os
+from subprocess import (
+    call,
+    check_output,
+)
+import sys
+from time import sleep
+
+import click
+import i3ipc
+
+from i3flash.client import FlashClient
+from i3flash.server import FlashServer
+
+# 0xffffffff
+MAX_OPACITY = 4294967295
+
+logging.basicConfig(level=os.environ.get('LOGLEVEL', 'INFO'))
+
+
+@click.command()
+@click.option('--opacity', '-o', default=0.9,
+              help='Opacity of the window during a flash')
+@click.option('--time', '-t', default=150, help='Flash time interval (in ms)')
+@click.option('--current', '-c', help='Flash the currently focused window')
+def cli(opacity, time, current):
+    '''Click command line interface group'''
+    # TODO: set logging parameters based on args
+    log('Checking command-line arguments...')
+    opacity = format_opacity(opacity)
+    time = time / 1000
+    log('Arguments are valid')
+
+    if current:
+        log('Initializing a client...')
+        FlashClient(opacity, time).run()
+    else:
+        log('Initializing the server...')
+        FlashServer(opacity, time).run()
+
+
+def format_opacity(opacity):
+    '''Convert the opacity paramater from decimal to int'''
+    if opacity > 1 or opacity < 0:
+        sys.exit("Invalid opacity argument, expected a decimal")
+
+    return int(opacity * MAX_OPACITY)
---
+++ i3flash/server.py
@@ -0,0 +1,84 @@
+from logging import info as log
+from multiprocessing.dummy import Pool
+from struct import unpack
+from time import sleep
+
+import i3ipc
+from socket import (
+    AF_UNIX,
+    socket,
+    SOCK_STREAM,
+)
+
+from i3flash.opacity import (
+    delete_opacity_property,
+    set_opacity,
+)
+
+
+class FlashServer(object):
+    def __init__(self, opacity, time, socketfile='/tmp/i3flash_socket'):
+        # TODO: Check for compton and issue warning
+        self.opacity = opacity
+        self.time = time
+        self.threads = Pool(2)
+
+        log('Establishing connection with i3...')
+        self.i3 = i3ipc.Connection()
+        log('Connection established')
+
+        self.focused_window = self.i3.get_tree().find_focused().window
+
+        log('Initializing a socket at %s', socketfile)
+        self.socket = socket(AF_UNIX, SOCK_STREAM)
+        self.socket.bind(socketfile)
+        log('Socket initialized')
+
+    def run(self):
+        self.threads.apply_async(self.monitor_focus)
+        self.threads.apply_async(self.monitor_client)
+
+    def flash_window(self, x_window_id):
+        '''Briefly decrease the opacity of a Xorg window'''
+        log('Flashing window %s', str(x_window_id))
+        set_opacity(x_window_id, opacity=self.opacity)
+
+        log('Waiting %ss...', self.time)
+        sleep(self.time)
+
+        # Setting opacity to the max wouldn't work if the window has the
+        # _NET_WM_OPAQUE_REGION defined, so we just delete the
+        # _NET_WM_WINDOW_OPACITY property to return to the default
+        delete_opacity_property(x_window_id)
+
+    def monitor_focus(self):
+        '''Wait for changes in focus and flash windows'''
+        def on_window_focus(_, event):
+            '''Change in focus hook'''
+            x_window_id = str(event.container.window)
+            self.flash_window(x_window_id)
+            self.focused_window = x_window_id
+            log('Waiting for focus shift...')
+
+        self.i3.on('window::focus', on_window_focus)
+        log('Waiting for focus shift...')
+        self.i3.main()
+
+    def monitor_client(self):
+        self.socket.listen(True)
+        connection, _ = self.socket.accept()
+        log('Waiting for client requests...')
+        while True:
+            data = connection.recv(8)
+            if data == b'':
+                raise RuntimeError('The client shutdown unexpectedly')
+            log('Received a request from the focus client...')
+            request = unpack('<L', data)[0]
+            if request == 1:
+                log('Flashing current window at client\'s request...')
+                self.flash_window(self.focused_window)
+            else:
+                raise RuntimeError('Received an unexpected response from the '
+                                   'client \n'
+                                   'Expected: 1 or None \n'
+                                   'Received: {}'.format(str(request)))
