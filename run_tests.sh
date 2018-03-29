f8436fd2430f856ad49bae92e5d80e0d5185be7c
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-03-29 15:42:15 -0700

    Removed dockerfile

--- run_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker build -t flashfocus .
-docker run --rm -p 8083:8083 -ti --name flashfocus -e DISPLAY=:0.0 flashfocus
--- test/docker_startup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-supervisord </dev/null &>/dev/null &
-sleep 1
-pytest
