be9941db4880bfc7a30cace13b3ac52de3958f96
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-03-29 19:01:06 -0700

    Updated automated testing to use docker

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t flashfocus .
-docker run --rm -p 8083:8083 -ti --name flashfocus -e DISPLAY=:0.0 flashfocus
+docker run --rm -p 8083:8083 -t --name flashfocus -e DISPLAY=:0.0 flashfocus
--- test/docker_startup.sh
+++ test/docker_startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 supervisord </dev/null &>/dev/null &
 sleep 1
-python3 -m pytest --failed-first --verbose -x --pdb --cov-report term-missing \
+python3 -m pytest --failed-first --verbose -x --cov-report term-missing \
     --cov="$PWD"
-python2 -m pytest --failed-first --verbose -x --pdb --cov-report term-missing \
+python2 -m pytest --failed-first --verbose -x --cov-report term-missing \
     --cov="$PWD"
