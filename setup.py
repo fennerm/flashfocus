7d10e95d7027e33b729cec85cdaea48e75ea975f
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-04-23 07:55:21 -0700

    Bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import fastentrypoints
 
 setup(
     name='flashfocus',
-    version='0.3.3',
+    version='0.3.4',
     author='Fenner Macrae',
     author_email='fmacrae.dev@gmail.com',
     description=("Simple focus animations for tiling window managers"),
