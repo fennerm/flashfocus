7f0e4afeb67382723896a9acce37794180ca1b07
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-05-05 16:19:10 -0700

    Added function for fetching window id and class

--- flashfocus/xutil.py
+++ flashfocus/xutil.py
@@ -10,3 +10,9 @@ def focus_shifted():
     if isinstance(event, PropertyNotifyEvent):
         return xpybutil.util.get_atom_name(event.atom) == '_NET_ACTIVE_WINDOW'
         return False
+
+
+def get_wm_class(window):
+    reply = xpybutil.util.get_property_value(
+        xpybutil.util.get_property(window, 'WM_CLASS').reply())
+    return reply[0], reply[1]
