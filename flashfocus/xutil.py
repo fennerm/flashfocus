26d1e2da89a0d244d0ac8c156b54770a89a74d90
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-05-06 11:31:41 -0700

    Added utility code for setting window opacity, and creating client message windows

--- flashfocus/xutil.py
+++ flashfocus/xutil.py
@@ -1,18 +1,52 @@
-"""Manipulate Xorg window opacity."""
-from xcffib.xproto import PropertyNotifyEvent
+"""Xorg utility code."""
+import xcffib
+import xcffib.xproto
 import xpybutil
+import xpybutil.ewmh
 import xpybutil.window
 
 
-def focus_shifted():
-    """Return True if focus shifted since last poll."""
-    event = xpybutil.conn.wait_for_event()
-    if isinstance(event, PropertyNotifyEvent):
-        return xpybutil.util.get_atom_name(event.atom) == '_NET_ACTIVE_WINDOW'
-        return False
+def create_message_window():
+    """Create a hidden window for sending X client-messages.
+
+    The window's properties can be used to send messages between threads.
+
+    Returns
+    -------
+    int
+        An X-window id.
+
+    """
+    setup = xpybutil.conn.get_setup()
+    window = xpybutil.conn.generate_id()
+    xpybutil.conn.core.CreateWindow(
+        depth=setup.roots[0].root_depth,
+        wid=window,
+        parent=xpybutil.root,
+        x=0, y=0, width=1, height=1, border_width=0,
+        _class=xcffib.xproto.WindowClass.InputOutput,
+        visual=setup.roots[0].root_visual,
+        value_mask=xcffib.xproto.CW.EventMask,
+        value_list=[xcffib.xproto.EventMask.PropertyChange],
+        is_checked=True).check()
+    return window
 
 
 def get_wm_class(window):
-    reply = xpybutil.util.get_property_value(
-        xpybutil.util.get_property(window, 'WM_CLASS').reply())
+    """Get the ID and class of a window
+
+    Returns
+    -------
+    Tuple[str, str]
+        (window id, window class)
+
+    """
+    reply = xpybutil.icccm.get_wm_class(window).reply()
     return reply[0], reply[1]
+
+
+def set_all_window_opacity(opacity):
+    """Set all visible window's opacity to `opacity`."""
+    for window in xpybutil.ewmh.get_client_list().reply():
+        xpybutil.ewmh.set_wm_window_opacity(window, opacity)
+    xpybutil.conn.flush()
--- test/test_xutil.py
+++ test/test_xutil.py
@@ -1,6 +1,21 @@
 """Testsuite for flashfocus.xutil."""
+from pytest import approx
+import xpybutil.icccm
+
 from flashfocus.xutil import *
 
 
 def test_get_wm_class(window):
     assert get_wm_class(window) == ('window1', 'Window1')
+
+
+def test_create_message_window():
+    window = create_message_window()
+    xpybutil.icccm.set_wm_name_checked(window, 'foo').check()
+
+
+def test_set_all_window_opacity(windows, list_only_test_windows):
+    set_all_window_opacity(0.5)
+    for window in windows:
+        assert (xpybutil.ewmh.get_wm_window_opacity(window).reply() ==
+                approx(0.5))
